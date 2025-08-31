bcce86a1ad0c24c352d33af11949da2dffef0b2e
Armaan Uppal <mupps@Armaans-MacBook-Pro-2.local>
2025-08-31 11:53:01 -0700

    fixed async issues with project page

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -4,14 +4,18 @@ import Image from "next/image";
 import Link from "next/link";
 import type { Metadata } from "next";
 
-interface Props {
-  params: {
-    slug: string;
-  };
+// interface Props {
+//   params: {
+//     slug: Promise<{slug: string}>;
+//   };
+// }
+interface PageProps {
+  params: Promise<{ slug: string }>;
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const project = projects.find((p) => p.slug === params.slug);
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { slug }: {slug: string} = await params;
+  const project = projects.find((p) => p.slug === slug);
   if (!project) return { title: "Project Not Found" };
 
   return {
@@ -26,8 +30,9 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function ProjectPage({ params }: Props) {
-  const project = projects.find((p) => p.slug === params.slug);
+export default async function ProjectPage({ params }: PageProps) {
+  const { slug } = await params;
+  const project = projects.find((p) => p.slug === slug);
   if (!project) notFound();
 
   return (
